6b16951f393600015cda4525e9cdaa7f93ab418a
fengshuai <ihanwei1314@163.com>
2022-09-01 01:17:20 +0800

    feat: 代码更新

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="w-full h-full layout-page pt-5">
+    <div
+      class="mx-5 px-16 py-5 bg-white rounded-xl overflow-hidden <md:px-4 <md:px-0"
+    >
+      <div class="md:flex">
+        <div class="md:flex-shrink-0">
+          <img
+            class="w-50 h-full object-cover <sm:h-60 <sm:w-auto m-auto"
+            src="./assets/vue.svg"
+            alt="Man looking at item at a store"
+          />
+        </div>
+        <div class="p-8">
+          <div
+            class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"
+          >
+            Case study
+          </div>
+          <a
+            href="#"
+            class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >Finding customers for your new business</a
+          >
+          <p class="mt-2 text-gray-500">
+            Getting a new business off the ground is a lot of hard work. Here
+            are five ideas you can use to find your first customers.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.layout-page {
+  min-height: 100vh;
+  background: #eef0f3;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteEslint from 'vite-plugin-eslint';
-import viteStylelint from 'vite-plugin-stylelint';
+import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteEslint(),
-    viteStylelint({
-      fix: true,
-      exclude: ['windicss', 'node_modules']
-    })
-  ]
+  plugins: [vue(), viteEslint(), WindiCSS()]
 });
