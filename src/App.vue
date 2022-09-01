3dc9ae695694ddf7944a334509420d7985a0191e
fengshuai <ihanwei1314@163.com>
2022-09-02 00:24:52 +0800

    feat: update code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="w-full h-full layout-page pt-5">
-    <div
-      class="mx-5 px-16 py-5 bg-white rounded-xl overflow-hidden <md:px-4 <md:px-0"
-    >
+    <div class="mx-5 px-16 py-5 bg-white rounded-xl overflow-hidden <md:px-4">
+      <div>
+        <div class="text-xl font-bold">商品：</div>
+        <div class="text-xl mt-0.5">PAYMENTLINK</div>
+      </div>
       <div class="md:flex">
         <div class="md:flex-shrink-0">
           <img
-            class="w-50 h-full object-cover <sm:h-60 <sm:w-auto m-auto"
+            class="w-100 h-full object-cover <sm:h-60 <sm:w-auto m-auto"
             src="./assets/vue.svg"
             alt="Man looking at item at a store"
           />
@@ -29,11 +31,19 @@
         </div>
       </div>
     </div>
+    <div class="mt-5 flex mx-5 justify-end <md:justify-start">
+      <button class="text-white rounded px-10 py-3 button <md:w-full">
+        提交
+      </button>
+    </div>
   </div>
 </template>
 <style>
 .layout-page {
   min-height: 100vh;
   background: #eef0f3;
+}
+.button {
+  background: #1787f9;
 }
 </style>
